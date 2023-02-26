d915922eaafc82e7c595bfec67fed3ead52874a0
rohsaurus <rohanparikh04@gmail.com>
2023-02-26 15:55:48 -0500

    Added progress bars for encryption and decryption, solving Add progress bars for encryption and decryption #5

--- cerberus/lib/pages/settings_screen.dart
+++ cerberus/lib/pages/settings_screen.dart
@@ -3,20 +3,33 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:settings_ui/settings_ui.dart';
-class SettingsScreen extends StatelessWidget {
+
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SettingsList(
         sections: [
           SettingsSection(
-            title: Text('Section'),
-            tiles: [
-              SettingsTile(
-                title: Text("Title"),
-                leading: Icon(Icons.ac_unit),
+            title: Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: Icon(Icons.language),
+                title: Text('Language'),
+                value: Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: Icon(Icons.format_paint),
+                title: Text('Enable custom theme'),
               ),
             ],
           ),
